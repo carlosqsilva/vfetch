4a21db41d39bf87655cf2d12f100029f1135dafa
Carlos Silva <carlosqsilva@outlook.com>
2023-11-11 13:35:42 -0300

    fix: multiline string

--- vfetch.v
+++ vfetch.v
@@ -429,9 +429,7 @@ fn new_system() ?System {
 }
 
 fn (mut sys System) get_song() {
-	data := os.execute("osascript -e \
-		'tell application \"Music\" to artist of current track as string \
-		& \" - \" & name of current track as string'")
+	data := os.execute("osascript -e 'tell application \"Music\" to artist of current track as string & \" - \" & name of current track as string'")
 
 	result := data.output.trim_space()
 	if data.exit_code == 0 && result.len > 0 {
