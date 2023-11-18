dd15738a27591f7ccec189c599b54fe7d715d2ad
Carlos Silva <carlosqsilva@outlook.com>
2023-11-18 13:26:44 -0300

    feat: added os name

--- vfetch.v
+++ vfetch.v
@@ -5,10 +5,11 @@ import term
 import time
 import json
 import strings
+import semver
 
 struct Result {
 	success bool
-	result string
+	result  string
 }
 
 fn success(result string) Result {
@@ -38,20 +39,23 @@ const (
 )
 
 struct Info {
-	logo []string = logo.split('\n')
+	logo     []string = logo.split('\n')
 	start_at int
 	left_gap int
-	image string
+	image    string
 mut:
-	gap int
-	index int
-	columns int
+	gap               int
+	index             int
+	columns           int
 	can_display_image bool
-	content strings.Builder = strings.new_builder(32768)
+	content           strings.Builder = strings.new_builder(32768)
 }
 
 fn (mut info Info) start() {
-	term.clear()
+	$if prod {
+		term.clear()
+	}
+
 	info.can_display_image = os.exists_in_system_path('kitty')
 
 	col, _ := term.get_terminal_size()
@@ -96,7 +100,7 @@ fn (mut info Info) add_line() {
 
 fn (mut info Info) write_string(str string) {
 	if info.index <= info.start_at {
-		for _ in 0..info.start_at {
+		for _ in 0 .. info.start_at {
 			info.add_line()
 		}
 	}
@@ -108,7 +112,7 @@ fn (mut info Info) write_string(str string) {
 
 fn (mut info Info) print() {
 	if info.index < info.logo.len {
-		for _ in info.index..info.logo.len {
+		for _ in info.index .. info.logo.len {
 			info.write_logo()
 			info.content.write_string('\n')
 		}
@@ -117,7 +121,7 @@ fn (mut info Info) print() {
 	if info.can_display_image && info.image != '' {
 		image_path := os.real_path(info.image)
 		if os.exists(image_path) {
-			os.execute('kitty +kitten icat --align left --place ${32}x${24}@1x1 $image_path')
+			os.execute('kitty +kitten icat --align left --place ${32}x${24}@1x1 ${image_path}')
 		}
 	}
 
@@ -125,49 +129,49 @@ fn (mut info Info) print() {
 }
 
 struct System {
-	user Result
-	uptime Result
-	memory Result
-	cpu Result
-	gpu Result
+	user       Result
+	uptime     Result
+	memory     Result
+	cpu        Result
+	gpu        Result
 	resolution Result
-	storage Result
-	os Result
-	term Result
-	machine Result
-	battery Result
+	storage    Result
+	os         Result
+	term       Result
+	machine    Result
+	battery    Result
 mut:
 	song Result
 }
 
 struct DisplayDevice {
-	model string [json: sppci_model]
-	cores string [json: sppci_cores]
-	device_type string [json: sppci_device_type]
+	model       string @[json: sppci_model]
+	cores       string @[json: sppci_cores]
+	device_type string @[json: sppci_device_type]
 }
 
 struct Display {
-	displays []DisplayDevice [json: SPDisplaysDataType]
+	displays []DisplayDevice @[json: SPDisplaysDataType]
 }
 
 struct StorageDevice {
-	free u64 [json: free_space_in_bytes]
-	size u64 [json: size_in_bytes]
-	mount string [json: mount_point]
+	free  u64    @[json: free_space_in_bytes]
+	size  u64    @[json: size_in_bytes]
+	mount string @[json: mount_point]
 }
 
 struct Storage {
-	storages []StorageDevice [json: SPStorageDataType]
+	storages []StorageDevice @[json: SPStorageDataType]
 }
 
 struct HardwareDevice {
-	machine string [json: machine_name]
+	machine string @[json: machine_name]
 }
 
 struct Machine {
-	hardware []HardwareDevice [json: SPHardwareDataType]
-	storages []StorageDevice [json: SPStorageDataType]
-	displays []DisplayDevice [json: SPDisplaysDataType]
+	hardware []HardwareDevice @[json: SPHardwareDataType]
+	storages []StorageDevice  @[json: SPStorageDataType]
+	displays []DisplayDevice  @[json: SPDisplaysDataType]
 }
 
 fn get_user(str string) Result {
@@ -210,11 +214,11 @@ fn get_memory(str string) Result {
 
 	for index, field in fields {
 		match field {
-			'APP' { app = fields[index + 1].u64()}
-			'WIRED' { wired = fields[index + 1].u64()}
-			'COMPRESSED' { compressed = fields[index + 1].u64()}
-			'PAGE_SIZE' { pages_size = fields[index + 1].u64()}
-			'MEMORY' { total = fields[index + 1].u64()}
+			'APP' { app = fields[index + 1].u64() }
+			'WIRED' { wired = fields[index + 1].u64() }
+			'COMPRESSED' { compressed = fields[index + 1].u64() }
+			'PAGE_SIZE' { pages_size = fields[index + 1].u64() }
+			'MEMORY' { total = fields[index + 1].u64() }
 			else { continue }
 		}
 	}
@@ -225,7 +229,7 @@ fn get_memory(str string) Result {
 
 	mem_used := ((app + wired + compressed) * pages_size / 1024 / 1024) / 1024
 
-	return success('${mem_used:.2} / $total GiB')
+	return success('${mem_used:.2} / ${total} GiB')
 }
 
 fn get_cpu(str string) Result {
@@ -235,7 +239,7 @@ fn get_cpu(str string) Result {
 	if start >= 0 && 'cpu' in re.group_map && 'cores' in re.group_map {
 		cpu := re.get_group_by_name(str, 'cpu')
 		cores := re.get_group_by_name(str, 'cores')
-		return success('$cpu $cores cores (physical)')
+		return success('${cpu} ${cores} cores (physical)')
 	}
 
 	return failure
@@ -261,7 +265,7 @@ fn get_gpu(data Machine) Result {
 		return success(model)
 	}
 
-	return success('$model ($cores cores)')
+	return success('${model} (${cores} cores)')
 }
 
 fn get_storage(data Machine) Result {
@@ -280,7 +284,7 @@ fn get_storage(data Machine) Result {
 		return failure
 	}
 
-	return success('$used / $total GB')
+	return success('${used} / ${total} GB')
 }
 
 fn get_resolution(str string) Result {
@@ -293,14 +297,13 @@ fn get_resolution(str string) Result {
 	if start >= 0 && 'res' in re.group_map && 'freq' in re.group_map {
 		res := re.get_group_by_name(str, 'res')
 		freq := re.get_group_by_name(str, 'freq')
-		return success('$res @ ${freq}Hz')
+		return success('${res} @ ${freq}Hz')
 	}
 
 	return failure
 }
 
 fn get_os(str string) Result {
-
 	mut product_name := ''
 	mut product_version := ''
 	mut product_build_version := ''
@@ -311,7 +314,7 @@ fn get_os(str string) Result {
 		match field {
 			'ProductName' { product_name = fields[index + 1] }
 			'ProductVersion' { product_version = fields[index + 1] }
-			'ProductBuildVersion' { product_build_version = fields[index + 1]}
+			'ProductBuildVersion' { product_build_version = fields[index + 1] }
 			else { continue }
 		}
 	}
@@ -320,7 +323,41 @@ fn get_os(str string) Result {
 		return failure
 	}
 
-	return success('$product_name $product_version ($product_build_version)')
+	get_os_name := fn (input string) ?string {
+		version := semver.coerce(input) or {
+			return none
+		}
+
+		return match true {
+			version.ge(semver.build(14, 0, 0)) { 'Sonoma' }
+			version.ge(semver.build(13, 0, 0)) { 'Ventura' }
+			version.ge(semver.build(12, 0, 0)) { 'Monterey' }
+			version.ge(semver.build(11, 0, 0)) { 'Big Sur' }
+			version.ge(semver.build(10, 15, 0)) { 'Catalina' }
+			version.ge(semver.build(10, 14, 0)) { 'Mojave' }
+			version.ge(semver.build(10, 13, 0)) { 'High Sierra' }
+			version.ge(semver.build(10, 12, 0)) { 'Sierra' }
+			version.ge(semver.build(10, 11, 0)) { 'El Capitan' }
+			version.ge(semver.build(10, 10, 0)) { 'Yosemite' }
+			version.ge(semver.build(10, 9, 0)) { 'Mavericks' }
+			version.ge(semver.build(10, 8, 0)) { 'Mountain Lion' }
+			version.ge(semver.build(10, 7, 0)) { 'Lion' }
+			version.ge(semver.build(10, 6, 0)) { 'Snow Leopard' }
+			version.ge(semver.build(10, 5, 0)) { 'Leopard' }
+			version.ge(semver.build(10, 4, 0)) { 'Tiger' }
+			version.ge(semver.build(10, 3, 0)) { 'Panther' }
+			version.ge(semver.build(10, 2, 0)) { 'Jaguar' }
+			version.ge(semver.build(10, 1, 0)) { 'Puma' }
+			version.ge(semver.build(10, 0, 0)) { 'Cheetah' }
+			else { none }
+		}
+	}
+
+	if os_name := get_os_name(product_version) {
+		return success('${product_name} ${os_name} ${product_version} (${product_build_version})')
+	} else {
+		return success('${product_name} ${product_version} (${product_build_version})')
+	}
 }
 
 fn get_term(str string) Result {
@@ -393,43 +430,59 @@ fn new_system() ?System {
 
 	for field in query.output.split('|') {
 		match true {
-			field.starts_with('USER') { user = get_user(field) }
-			field.starts_with('UPTIME') { uptime = get_uptime(field) }
-			field.starts_with('MEMORY') { memory = get_memory(field) }
-			field.starts_with('CPU') { cpu = get_cpu(field) }
-			field.starts_with('RESOLUTION') { resolution = get_resolution(field) }
-			field.starts_with('OS') { myos = get_os(field) }
-			field.starts_with('TERM') { myterm = get_term(field) }
-			field.starts_with('BATTERY') { battery = get_battery(field) }
+			field.starts_with('USER') {
+				user = get_user(field)
+			}
+			field.starts_with('UPTIME') {
+				uptime = get_uptime(field)
+			}
+			field.starts_with('MEMORY') {
+				memory = get_memory(field)
+			}
+			field.starts_with('CPU') {
+				cpu = get_cpu(field)
+			}
+			field.starts_with('RESOLUTION') {
+				resolution = get_resolution(field)
+			}
+			field.starts_with('OS') {
+				myos = get_os(field)
+			}
+			field.starts_with('TERM') {
+				myterm = get_term(field)
+			}
+			field.starts_with('BATTERY') {
+				battery = get_battery(field)
+			}
 			field.starts_with('MISC') {
-				data := get_misc(field) or {
-					continue
-				}
+				data := get_misc(field) or { continue }
 				machine = get_machine(data)
 				storage = get_storage(data)
 				gpu = get_gpu(data)
 			}
-			else {continue}
+			else {
+				continue
+			}
 		}
 	}
 
 	return System{
-		user: user,
-		uptime: uptime,
-		memory: memory,
-		cpu: cpu,
-		gpu: gpu,
-		resolution: resolution,
-		storage: storage,
-		os: myos,
-		term: myterm,
-		machine: machine,
+		user: user
+		uptime: uptime
+		memory: memory
+		cpu: cpu
+		gpu: gpu
+		resolution: resolution
+		storage: storage
+		os: myos
+		term: myterm
+		machine: machine
 		battery: battery
 	}
 }
 
 fn (mut sys System) get_song() {
-	data := os.execute("osascript -e 'tell application \"Music\" to artist of current track as string & \" - \" & name of current track as string'")
+	data := os.execute('osascript -e \'tell application "Music" to artist of current track as string & " - " & name of current track as string\'')
 
 	result := data.output.trim_space()
 	if data.exit_code == 0 && result.len > 0 {
@@ -446,7 +499,11 @@ fn main() {
 
 	mut sys := new_system()?
 
-	mut info := Info{start_at: 2, left_gap: 4, image: custom_image}
+	mut info := Info{
+		start_at: 2
+		left_gap: 4
+		image: custom_image
+	}
 
 	info.start()
 	info.write_string(term.bright_yellow('╭────────────╮'))
@@ -456,45 +513,44 @@ fn main() {
 	}
 
 	if sys.machine.success {
-		info.write_string(term.bright_yellow('│ MACHINE    │ : $sys.machine.result'))
+		info.write_string(term.bright_yellow('│ MACHINE    │ : ${sys.machine.result}'))
 	}
 
 	if sys.os.success {
-		info.write_string(term.bright_yellow('│ OS         │ : $sys.os.result'))
+		info.write_string(term.bright_yellow('│ OS         │ : ${sys.os.result}'))
 	}
 
 	if sys.cpu.success {
-		info.write_string(term.bright_yellow('│ CPU        │ : $sys.cpu.result'))
+		info.write_string(term.bright_yellow('│ CPU        │ : ${sys.cpu.result}'))
 	}
 
 	if sys.gpu.success {
-		info.write_string(term.bright_yellow('│ GPU        │ : $sys.gpu.result'))
+		info.write_string(term.bright_yellow('│ GPU        │ : ${sys.gpu.result}'))
 	}
 
 	if sys.resolution.success {
-		info.write_string(term.bright_yellow('│ RESOLUTION │ : $sys.resolution.result'))
+		info.write_string(term.bright_yellow('│ RESOLUTION │ : ${sys.resolution.result}'))
 	}
 
 	if sys.memory.success {
-		info.write_string(term.bright_yellow('│ MEMORY     │ : $sys.memory.result'))
+		info.write_string(term.bright_yellow('│ MEMORY     │ : ${sys.memory.result}'))
 	}
 
 	if sys.storage.success {
-		info.write_string(term.bright_yellow('│ STORAGE    │ : $sys.storage.result'))
+		info.write_string(term.bright_yellow('│ STORAGE    │ : ${sys.storage.result}'))
 	}
 
 	if sys.uptime.success {
-		info.write_string(term.bright_yellow('│ UPTIME     │ : $sys.uptime.result'))
+		info.write_string(term.bright_yellow('│ UPTIME     │ : ${sys.uptime.result}'))
 	}
 
 	if sys.battery.success {
-		info.write_string(term.bright_yellow('│ BATTERY    │ : $sys.battery.result'))
+		info.write_string(term.bright_yellow('│ BATTERY    │ : ${sys.battery.result}'))
 	}
 
 	if sys.term.success {
-		info.write_string(term.bright_yellow('│ TERMINAL   │ : $sys.term.result'))
+		info.write_string(term.bright_yellow('│ TERMINAL   │ : ${sys.term.result}'))
 	}
-
 
 	dot := '■'
 	info.write_string(term.bright_yellow('├────────────┤'))
